492cfc598a0e48297968a107ace170358ddcd392
Julius Robert Javier <juliusrobertjavier@gmail.com>
2018-11-02 16:14:25 +0800

    module8

--- routes/login.js
+++ routes/login.js
@@ -18,25 +18,25 @@ router.get('/', function(req, res, next) {
 
 
 // create admin
-router.post('/', function(req, res, next) {
-  bcrypt.hash(req.body.password, 10).then(hash => {
-    console.log(hash)
-    const query = {
-      text: `INSERT INTO users (username,password,is_admin,user_type, first_name) VALUES ($1,$2,true,'admin','Administrator')`,
-      values: [req.body.username,  hash]
-    }
-    db.query(query).then(data => {
-      console.log(data)
-      res.redirect('/login')
-    }).catch(e => {
-      console.log(e)
-      res.redirect('/login')
-    })
-  }).catch(e => {
-    console.log(e)
-    res.redirect('/login')
-  })
-})
+// router.post('/', function(req, res, next) {
+  // bcrypt.hash(req.body.password, 10).then(hash => {
+    // console.log(hash)
+    // const query = {
+      // text: `INSERT INTO users (username,password,is_admin,user_type, first_name) VALUES ($1,$2,true,'admin','Administrator')`,
+      // values: [req.body.username,  hash]
+    // }
+    // db.query(query).then(data => {
+      // console.log(data)
+      // res.redirect('/login')
+    // }).catch(e => {
+      // console.log(e)
+      // res.redirect('/login')
+    // })
+  // }).catch(e => {
+    // console.log(e)
+    // res.redirect('/login')
+  // })
+// })
 
 
 
