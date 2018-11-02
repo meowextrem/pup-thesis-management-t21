52631d482185f5eec7f2f040a9238003ff5d210a
Julius Robert Javier <juliusrobertjavier@gmail.com>
2018-11-02 16:11:04 +0800

    module8

--- db/index.js
+++ db/index.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg')
 
 const pool = new Pool({
-  database: 'dd1ictctqi0cm3',
-  user: 'oxhzdvnaspxdtt',
-  password: 'd90fcd2cdd91a8ba06b2ac72f68f0cca6e9eb38bde8d4eb08e6d92b504514cb9',
-  host: 'ec2-54-227-240-7.compute-1.amazonaws.com',
+  database: 'd8hqanjf2e1gcp',
+  user: 'wqqzaywjffckab',
+  password: 'cbdaaa01f0e53e8f95121f5417ef7ff3223ecd2eac70d5c3a88670d9726cc92a',
+  host: 'ec2-54-225-110-152.compute-1.amazonaws.com',
   port: 5432,
   ssl: true
 	  // database: 'postgres',
